02e321d38f4c0acf2cd9cc39b0b8a2fb74383dc7
Ícaro Novo de Oliveira <icaronovo@outlook.com>
2023-11-29 01:25:04 -0800

    added the map to the home screen

--- app/src/main/java/com/ebookfrenzy/csis4175_groupproject/HomePage.java
+++ app/src/main/java/com/ebookfrenzy/csis4175_groupproject/HomePage.java
@@ -1,13 +1,17 @@
 package com.ebookfrenzy.csis4175_groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
 
 public class HomePage extends AppCompatActivity {
 
@@ -18,6 +22,20 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+//        Bundle mBundle = new Bundle();
+
+//        ArrayList<String> currentLocation = new ArrayList<>();
+//        currentLocation.add("49.2488");
+//        currentLocation.add("-122.9805");
+
+//        mBundle.putStringArrayList("coordinates", currentLocation);
+//        final androidx.fragment.app.FragmentManager mFragmentManager = getSupportFragmentManager();
+//        final androidx.fragment.app.FragmentTransaction homePageMapsFragmentTransaction
+//                = mFragmentManager.beginTransaction();
+//        final HomePageMapsFragment homePageMapsFragment = new HomePageMapsFragment();
+//
+//        homePageMapsFragment.setArguments(mBundle);
+//        homePageMapsFragmentTransaction.add(R.id.fragment_container_view, homePageMapsFragment).commit();
 
         signOutButton = findViewById(R.id.signOutButton);
         sightingsButton = findViewById(R.id.toSightings);
