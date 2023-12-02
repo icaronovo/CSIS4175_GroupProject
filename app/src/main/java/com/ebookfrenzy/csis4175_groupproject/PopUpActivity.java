613cb70027b19ef2bfb883222f675a5491c0bd19
Icaro Novo de Oliveira <icaronovo@outlook.com>
2023-12-01 16:41:41 -0800

    Added exit button on pop up screen

--- app/src/main/java/com/ebookfrenzy/csis4175_groupproject/PopUpActivity.java
+++ app/src/main/java/com/ebookfrenzy/csis4175_groupproject/PopUpActivity.java
@@ -2,9 +2,12 @@ package com.ebookfrenzy.csis4175_groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,6 +19,7 @@ public class PopUpActivity extends AppCompatActivity {
 
     private double latitude;
     private double longitude;
+    private Button exitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +39,15 @@ public class PopUpActivity extends AppCompatActivity {
             googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title(animalSighting.getAnimalType()));
             googleMap.getUiSettings().setZoomControlsEnabled(true);
             googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12.0f));
+        });
+
+        exitButton = findViewById(R.id.exitButton);
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = getIntent();
+                finish();
+            }
         });
     }
 
